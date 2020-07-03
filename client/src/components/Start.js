41b6dd24283c820090b0a70f6d446543be3d6e56
Manuel <mbuergel@gmx.de>
2020-07-03 03:47:54 +0200

    styled startpage and login, updated profile search bar to get rid of the error

--- client/src/components/Start.js
+++ client/src/components/Start.js
@@ -13,23 +13,28 @@ class Start extends Component {
         <Link to="/profile">Profile</Link>?
       </div>
     ) : (
-      <Row fluid className="startFullHeight">
-        <Col sm="auto" className="align-self-center">
-          <Row>
-            <Col className="text-center">
-              <h1 className="display-3 display-1-md text-white">RECORD BOX</h1>
+      <Container fluid className="startFullHeight">
+        <Row className="h-100 align-items-center justify-content-center">
+          <Col xs="10" md="auto" className="fadeIn text-center">
+            <Col className="display-4 text-dark">RECORD BOX</Col>
+            <Col className="h5 text-dark text-monospace my-4">
+              Easily manage and track all your vinyl
             </Col>
-          </Row>
-          <Row className="text-center">
-            <Col>
-              <Link to="/signup">Signup</Link>
-            </Col>
-            <Col>
-              <Link to="/login">Login</Link>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+            <Row className="my-4 justify-content-center">
+              <Col xs="auto">
+                <Link className="text-info" to="/login">
+                  Login
+                </Link>
+              </Col>
+              <Col xs="auto">
+                <Link className="text-info" to="/signup">
+                  Signup
+                </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
