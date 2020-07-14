921e4d0de9af08f7d60955e92faf55908158e1d9
Manuel <mbuergel@gmx.de>
2020-07-14 21:48:32 +0200

    cleanup and some scan page styling

--- client/src/components/VerifyEmail.js
+++ client/src/components/VerifyEmail.js
@@ -1,13 +1,11 @@
-import React, { Component }from 'react'
+import React from "react";
 
-function VerifyEmail ()
-{
-    return(
-      <div>
-        <h5>Check your E-mail box to verify!</h5>
-      </div>
-    )
-  
+function VerifyEmail() {
+  return (
+    <div>
+      <h5>Check your E-mail box to verify!</h5>
+    </div>
+  );
 }
 
-export default VerifyEmail
+export default VerifyEmail;
